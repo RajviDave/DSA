0cab96362d671a6ba88671742f314f785f0052b0
RajviDave <rajvidave22@gmail.com>
2026-05-16 21:03:42 +0530

    largest element

--- CPP/largest_element.cpp
+++ CPP/largest_element.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main(){
     vector<int> arr;
-    arr = {6,7,2,1,4};
+    arr = {4,3,6};
 
     int max_element = arr[0];
+    int second_max_element=-1;
     for (int i=0;i<arr.size();i++){
         if(arr[i]>max_element){
             max_element=arr[i];
         }
     }
-    cout<<"The maximum element is ="<<max_element;
+
+    for (int j=0;j<arr.size();j++){
+        if(arr[j]>second_max_element && arr[j]!=max_element){
+            second_max_element=arr[j];
+        }
+    }
+    cout<<"The second maximum element is ="<<second_max_element;
     return 0;
 }
